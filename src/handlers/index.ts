a7572f0ae76b32d1f8189a8e997e4aa60fcd3dd5
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-05-18 12:19:48 +0530

    all routs battle tested

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,6 +1,6 @@
 import prisma from "../db";
 
-export const getPosts = async () => {
+const getPosts = async () => {
     try {
         return await prisma.post.findMany({
             orderBy: {
@@ -10,4 +10,68 @@ export const getPosts = async () => {
     } catch (error) {
         console.log(`Error in getPosts: ${error}`);
     }
+};
+
+const getPostsById = async (id: number) => {
+    try {
+        return await prisma.post.findUnique({
+            where: {
+                Id: Number(id)
+            }
+        });
+    } catch (error) {
+        console.log(`Error in getPostsById: ${error}`);
+    }
+};
+
+const createPosts = async (title: string, content: string) => {
+    try {
+        return await prisma.post.create({
+            data: {
+                title: title,
+                content: content,
+                createdAt: new Date(Date.now())
+            }
+        });
+    } catch (error) {
+        console.log(`Error in createPosts: ${error}`);
+    }
+};
+
+const updatePosts = async (id: number, title: string, content: string) => {
+    try {
+        return await prisma.post.updateMany({
+            where: {
+                Id: Number(id)
+            },
+            data: {
+                title: title,
+                content: content,
+                updatedAt: new Date(Date.now())
+            }
+        });
+    } catch (error) {
+        console.log(`Error in updatePosts: ${error}`);
+    }
+};
+
+
+const deletePost = async (id: number) => {
+    try {
+        return await prisma.post.delete({
+            where: {
+                Id: Number(id)
+            }
+        });
+    } catch (error) {
+        console.log(`Error in deletePosts: ${error}`);
+    }
+};
+
+export {
+    getPosts,
+    getPostsById,
+    createPosts,
+    updatePosts,
+    deletePost
 };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
 import { Elysia } from 'elysia';
-import { getPosts } from '../handlers';
+import { createPosts, deletePost, getPosts, getPostsById, updatePosts } from '../handlers';
 
 const routes = new Elysia({ prefix: '/posts' })
     .get('/', () => getPosts())
-    .get('/id', () => 'Get Post by id')
-    .post('/', () => 'Create Post')
-    .put('/id', () => 'Update Post by id')
-    .delete('/id', () => 'Delete Post by id');
+    .get('/:id', ({ params }: { params: { id: number; }; }) => getPostsById(params.id))
+    .post('/', ({ body }: { body: { title: string, content: string; }; }) => createPosts(body.title, body.content))
+    .patch('/:id', ({ params, body }: { params: { id: number; }; body: { title: string; content: string; }; }) => { updatePosts(params.id, body.title, body.content); })
+    .delete('/:id', ({ params }: { params: { id: number; }; }) => deletePost(params.id));
 
 export default routes;
